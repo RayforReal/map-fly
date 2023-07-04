205d7c834419e95378562264faa5411c5576a176
leijiaxin <jiaxin.lei@tophant@.com>
2023-07-04 16:11:59 +0800

    feat: project init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="container">
+        1
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped>
+#container{
+    width: 100%;
+    height: 100%;
+}
+</style>
