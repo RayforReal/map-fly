11cde07431538bf72b86c07935fc6c7c44d0dd9a
leijiaxin <jiaxin.lei@tophant@.com>
2023-07-06 10:50:01 +0800

    1

--- src/store/earthMap.ts
+++ src/store/earthMap.ts
@@ -4,6 +4,7 @@ import { Scene, Group } from 'three';
 import { convertTo3D } from './dataUtils';
 import worldJson from '../data/world.json';
 import starsImg from '../assets/images/stars.png';
+import { Position } from "geojson";
 
 export class EarthMap {
     scene: Scene
@@ -64,70 +65,52 @@ export class EarthMap {
         this.scene.add(new THREE.Points(starsGeometry, starsMaterial))
     }
 
-    // 创建国家线条
+    // 创建国家轮廓
     createCountryLine() {
-        // 创建线条网格材质
-        const material = new THREE.LineBasicMaterial({ color: config.countryLineColor });
+        const allGroup:Group[] = []
         // 遍历国家数据
         worldJson.features.forEach(country => {
-            const geometry = new THREE.BufferGeometry();
+            // 单轮廓国家
             if (country.geometry.type === "Polygon") {
-                // 提取国家的几何数据
                 const positions = country.geometry.coordinates.flat();
-                // 创建顶点数组
-                const vertices = [];
-                for (let i = 0; i < positions.length; i++) {
-                    const longitude = positions[i][0];
-                    const latitude = positions[i ][1];
-                    const vertex = convertTo3D(latitude, longitude, config.mapRadius);
-                    vertices.push(vertex.x, vertex.y, vertex.z);
-                }
-                // 设置几何体的位置属性
-                geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-                // 创建线条网格
-                const line = new THREE.Line(geometry, material);
-                // 添加线条网格到场景中
-                this.scene.add(line);
+                allGroup.push(this.createLine(positions, country.properties.name))
             } else {
+                // 多个轮廓国家 中国-台湾
                 const multiPositions = country.geometry.coordinates;
                 for (let j = 0; j < multiPositions.length; j++) {
                     const positions = multiPositions[j];
-                    // 创建几何体
-                    const geometry = new THREE.BufferGeometry();
-                    // 创建顶点数组
-                    const vertices = [];
                     for (let i = 0; i < positions.length; i++) {
-                        const polygon = positions[i];
-                        for (let k = 0; k < polygon.length; k++) {
-                            const longitude = polygon[k][0];
-                            const latitude = polygon[k][1];
-                            const vertex = convertTo3D(latitude, longitude, config.mapRadius);
-                            vertices.push(vertex.x, vertex.y, vertex.z);
-                        }
+                        allGroup.push(this.createLine(positions[i], country.properties.name))
                     }
-                    // 设置几何体的位置属性
-                    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-                    // 创建线条网格
-                    const line = new THREE.Line(geometry, material);
-                    // 添加线条网格到场景中
-                    this.scene.add(line);
                 }
-
             }
         });
+        this.scene.add(...allGroup)
     }
 
-    transitionCoordinates(coordinates) {
-        coordinates.forEach(coordinate => {
-            coordinate.forEach(coordinateItem => {
-                // 将经纬度转换为三维坐标并将点添加到地球上
-                const position = convertTo3D(coordinateItem[0], coordinateItem[0], config.mapRadius);
-                const pointGeometry = new THREE.SphereGeometry(0.2, 8, 8);
-                const pointMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-                const point = new THREE.Mesh(pointGeometry, pointMaterial);
-                point.position.copy(position);
-            })
-        })
+    createLine(positions:Position[], countryName:string) {
+        // 创建几何体
+        const geometry = new THREE.BufferGeometry();
+        // 创建线条网格材质
+        const material = new THREE.LineBasicMaterial({ color: config.countryLineColor });
+        // 创建顶点数组
+        const vertices = [];
+        for (let i = 0; i < positions.length; i++) {
+            const longitude = positions[i][0];
+            const latitude = positions[i ][1];
+            const vertex = convertTo3D(latitude, longitude, config.mapRadius);
+            vertices.push(vertex.x, vertex.y, vertex.z);
+        }
+        // 设置几何体的位置属性
+        geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+        // 创建线条网格
+        const line = new THREE.Line(geometry, material);
+        const countryGroup = new Group();
+        if (countryName) {
+            countryGroup.name = `countryGroup-${countryName}`;
+        }
+        countryGroup.add(line)
+        return countryGroup;
     }
 }
 
